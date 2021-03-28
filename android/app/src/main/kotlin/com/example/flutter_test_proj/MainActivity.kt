a407ee18ea156eed423e6a61e27b67e33a34b465
manlemmi <dr.radov2010@gmail.com>
2021-03-28 15:03:20 +0300

    sthm going on

---
+++ android/app/src/main/kotlin/com/example/flutter_test_proj/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_test_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: MyPage()));
+}
+
+class MyPage extends StatefulWidget {
+  @override
+  _MyPageState createState() => new _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> {
+  final Random _random = Random();
+
+  Color _color = Color(0xFFFFFFFF);
+
+  void changeColor() {
+    setState(() {
+      _color = Color.fromARGB(
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+        _random.nextInt(256),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: InkWell(
+        onTap: changeColor,
+        child: Container(
+          color: _color,
+          child: Align(
+            alignment: Alignment.center,
+            child: Text('Hi there!'),
+          ),
+        ),
+      ),
+    );
+  }
+}
